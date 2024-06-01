0cede585aaf4c391241e4c88c7497175ef95d7d4
Valeh Ismayilov <valeh.ismayilov@outlook.com>
2024-06-02 01:24:28 +0400

    initial

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-4xl font-semibold">Vite + ShadCn + TailwindCSS</h1>
+    </div>
+  );
+};
+
+export default App;
